ce43eab63c513d5c414af56444be2b9e905d5889
BLACKPOLERBEAR3 <mikeforg@gmail.com>
2020-07-08 18:27:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.get ("/test", (request, response) -> {
+    response.send("Hello from API");
+)}
+
 app.listen(9999, () => {
     console.log("App is listening!");
 })
